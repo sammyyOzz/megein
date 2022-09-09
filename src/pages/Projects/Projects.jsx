5abc1049ac4e07b80f0f849657da25a304258dae
Sammy <sammyoziegbe@gmail.com>
2022-09-09 16:06:25 +0100

    updates, added more images

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,12 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, lazy, Suspense } from 'react'
 import TopSection from '../../components/TopSection/TopSection'
 import projectsPageImage from '../../assets/images/projectsPage.png'
 import './Projects.css'
 import Project from '../../components/Project/Project'
-import { projectsData, projectImages1, projectImages2, projectImages3 } from './projectsData'
+import { 
+    projectsData, 
+    projectImages1, 
+    projectImages2, 
+    projectImages3, 
+    otherImages 
+} from './projectsData'
 import women2 from '../../assets/images/women2.png'
 import { Grid } from '@mui/material'
 import { useParams, useHistory } from 'react-router-dom'
+import Skeleton from '@mui/material/Skeleton';
+
+const Video = lazy(() => import('../../components/Video/Video'));
+
+const Loader = () => (
+    <Skeleton
+        sx={{ 
+            bgcolor: 'grey.900',
+            width: '100%',
+            height: '350px'
+        }}
+        variant="rectangular"
+    />
+)
+
 
 
 function Projects() {
@@ -72,14 +93,114 @@ function Projects() {
                         <Project title={projectsData[1].title} date={projectsData[1].date} text={projectsData[1].text} image={women2} />
                         <Project title={projectsData[2].title} date={projectsData[2].date} text={projectsData[2].text} image={women2} />
                         <Project title={projectsData[3].title} date={projectsData[3].date} text={projectsData[3].text} image={women2} />
+                        <Project link={projectsData[4].link} date={projectsData[4].date} linkText={projectsData[4].linkText} image={women2} />
+                        <Project link={projectsData[5].link} date={projectsData[5].date} linkText={projectsData[5].linkText} image={women2} />
                     </Grid>
                     <Grid item xs={1} md={3} />
                 </Grid>
             )}
 
+            { page === '5' && (
+                <>
+                    <h2 className="projects__title">
+                        GENDER IMPLICATIONS OF NEWS OUTPUT: REINFORCING THE KNOWLEDGE BASE OF MEDIA PRACTITIONERS
+                    </h2>
+
+                    <h3 className="projects__subtitle">
+                        PRODUCED BY
+                    </h3>
+
+                    <h3 className="projects__subtitle">
+                        MEDIA AND GENDER ENLIGHTENMENT INITIATIVE (MEGEIN)
+                    </h3>
+
+                    <h3 className="projects__subtitle">
+                        (Watching the watchdog)
+                    </h3>
+
+                    <h3 className="projects__subtitle">
+                        In Collaboration With
+                    </h3>
+
+                    <h3 className="projects__subtitle">
+                        WORLD ASSOCIATION FOR CHRISTIAN COMMUNICATION (WACC) 
+                    </h3>
+
+                    <h3 className="projects__subtitle">
+                        © media and gender enlightenment initiative, 2018
+                    </h3>
+
+            
+                    <p className="projects__text">Incontrovertibly, the slow rate of African nations at covering the yawning gap of inequality between men and women representations in the news media has become worrisome. This is as it affects the need to overcome the daunting challenges of mainstreaming gender for the sustenance of national development programmes prevailing in African nations, of which Nigeria is part.</p>
+                    <p className="projects__text">
+                        Reinforcing this view, the global report of GMMP 2020 (www.gmmp2020) states that “full gender equality on numerical counts, however, is insufficient without improvement in the quality of journalism from a gender perspective”. It was this idea that informed the collaborative initiative between MEGEIN and WACC that resulted in the training of editors and senior reporters in Nigeria in gender perspective reporting in 2008. Unfortunately, media monitoring in subsequent years have not shown significant improvement. 
+                    </p>
+                    <p className="projects__text">
+                        Findings of Global Media Monitoring Project (GMMP) carried out over the years have persistently revealed the poor presence of women, including the silencing of their voices in mainstream and social media. Recalling the shocking revelations of Nigeria’s national report of GMMP 2005, Fab-Ukozor and Nwodu state that “…overall result here showed that a total of 42 females were news subjects as against 206 males used as news subjects”. (GMMP Report, 2005: p.15). The implication of the above statistics show that the views of men were more highlighted in the news since they were widely dominant as news subjects than women.
+                    </p>
+                    <p className="projects__text">
+                        Subsequent GMMP reports also replicated similar findings. It is disheartening to note that 2010 and 2015 results of GMMP showed 24% of women as news subjects and 76% of men as news subjects. This revealed that there was no significant improvement in the visibility of women. Inasmuch as the result disclosed a slight increase, judging from previous report of 2005, the fact that stagnation was recorded did not augur well for the pursuit of gender equality and balance. 
+                    </p>
+                    <p className="projects__text">
+                        Between 2015 and 2020, the needle edged one point forward to 25% in the representation of women as subjects and sources which was equally discouraging. The single point improvement is the first since 2010 and is most visible in broadcast media (www.gmmp2020). 
+                    </p>
+                    <p className="projects__text">
+                        However, it is important to note that overall findings of GMMP 2020 showed interesting result regarding women’s presence in the news in North America and the Pacific. The report reveal that …women’s presence in the regions… has surpassed the critical 30% threshold in both digital and legacy media, unlike the result in Africa which fell below the global averages across all media types monitored. Similar results were observed in Asia and the Middle East in print and broadcast news (www.gmmp2020).
+                    </p>
+                    <p className="projects__text">
+                        Import of the aforementioned results indicate that journalists in Africa, Asia and Middle East need to have diversified and in-depth knowledge of gender mainstream in order to assist the achievement of sustainable development goals in the regions. Hence, this workshop is targeted at reinforcing the knowledge base of media practitioners in Southeast, Nigeria regarding the gender implications of news output. It is therefore, the expectation of MEGEIN that after this exercise, news stories in Nigeria will begin to represent and portray women in the same degree that they participate in reality to engender the system.
+                    </p>
+                    <p className="projects__text">
+                        We are grateful to WACC for sponsoring this workshop and remain thankful to Imo State University, Owerri for providing us with accommodation to offer our services in the Department of Mass communication, Faculty of Social Sciences.
+                    </p>
+
+                    <h3 className="projects__subtitle">
+                        Dr. Alexander Chima Onyebuchi
+                    </h3>
+                    <h3 className="projects__subtitle">
+                        Project Manager
+                    </h3>
+                    
+                </>
+            )}
+
+            { page === '6' && (
+                <Grid container className="projects">
+                    <Grid item xs={1} md={3} />
+                    <Grid item xs={10} md={6}>
+                        <Suspense fallback={<Loader />}>
+                            <Video />
+                        </Suspense>
+                        
+                    </Grid>
+                    <Grid item xs={1} md={3} />
+                </Grid>
+            )}
+
+            { page === '7' && (
+                <>
+                    <h2 className="projects__title">Other Images</h2>
+                    <div className="projects__images-container">
+                        { otherImages.map((image, i) => (
+                            <img 
+                                key={i} 
+                                className="projects__image" 
+                                src={image} 
+                                alt="" 
+                                loading="lazy"
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
+
             <div className="projects__nav-button-container">
-                { Array(4).fill().map((item, i) => (
-                    <button className={`projects__nav ${page === String(i + 1) && 'projects__nav-active' }`} onClick={() => handleNav(i + 1)}>{ i + 1 }</button>
+                { Array(7).fill().map((item, i) => (
+                    <button 
+                        key={i}
+                        className={`projects__nav ${page === String(i + 1) && 'projects__nav-active' }`} 
+                        onClick={() => handleNav(i + 1)}>{ i + 1 }
+                    </button>
                 ))}
             </div>
         </Fragment>
--- src/pages/Projects/projectsData.js
+++ src/pages/Projects/projectsData.js
@@ -22,6 +22,37 @@ import violence7 from '../../assets/images/Media Chat and Press Discussion.jpg'
 import violence8 from '../../assets/images/Say No To VAW Flier.jpg'
 import violence9 from '../../assets/images/WACCglobal-tall_EN copy Logo.jpg'
 
+import otherImages1 from '../../assets/images/other-images/1.jpg'
+import otherImages2 from '../../assets/images/other-images/2.jpg'
+import otherImages3 from '../../assets/images/other-images/3.jpg'
+import otherImages4 from '../../assets/images/other-images/4.jpg'
+import otherImages5 from '../../assets/images/other-images/5.jpg'
+import otherImages6 from '../../assets/images/other-images/6.jpg'
+import otherImages7 from '../../assets/images/other-images/7.jpg'
+import otherImages8 from '../../assets/images/other-images/8.jpg'
+import otherImages9 from '../../assets/images/other-images/9.jpg'
+import otherImages10 from '../../assets/images/other-images/10.jpg'
+import otherImages11 from '../../assets/images/other-images/11.jpg'
+import otherImages12 from '../../assets/images/other-images/12.jpg'
+import otherImages13 from '../../assets/images/other-images/13.jpg'
+import otherImages14 from '../../assets/images/other-images/14.jpg'
+import otherImages15 from '../../assets/images/other-images/15.jpg'
+import otherImages16 from '../../assets/images/other-images/16.jpg'
+import otherImages17 from '../../assets/images/other-images/17.jpg'
+import otherImages18 from '../../assets/images/other-images/18.jpg'
+import otherImages19 from '../../assets/images/other-images/19.jpg'
+import otherImages20 from '../../assets/images/other-images/20.jpg'
+import otherImages21 from '../../assets/images/other-images/21.jpg'
+import otherImages22 from '../../assets/images/other-images/22.jpg'
+import otherImages23 from '../../assets/images/other-images/23.jpg'
+import otherImages24 from '../../assets/images/other-images/24.jpg'
+import otherImages25 from '../../assets/images/other-images/25.jpg'
+import otherImages26 from '../../assets/images/other-images/26.jpg'
+import otherImages27 from '../../assets/images/other-images/27.jpg'
+import otherImages28 from '../../assets/images/other-images/28.jpg'
+import otherImages29 from '../../assets/images/other-images/29.jpg'
+
+
 export const projectsData = [
     {
         title: 'Gender Perspective Reporting',
@@ -46,7 +77,21 @@ export const projectsData = [
         date: '2021',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum quis sapien id volutpat. Aenean eu tincidunt eros. In hac habitasse platea dictumst. Integer pulvinar sem odio, nec interdum quam eleifend vitae. Duis volutpat facilisis maximus. Nulla vulputate in erat ac viverra. Nunc tempor nunc convallis diam posuere, sit amet porttitor ligula consequat. Nam sed finibus est, vitae dignissim purus. Ut velit urna, iaculis non vestibulum vel, finibus nec justo. Morbi id tortor condimentum, auctor odio vitae, imperdiet magna. In hac habitasse platea dictumst. Nam vitae odio pellentesque, eleifend orci a, ullamcorper ante. Phasellus aliquet accumsan magna vitae laoreet. Praesent placerat magna ut lacinia ultricies. Donec auctor fermentum nulla. Curabitur accumsan dolor commodo tristique molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec dictum porttitor ex. Vestibulum ornare gravida tincidunt. Ut consectetur nunc eget tristique efficitur. Phasellus viverra pulvinar lorem ac consectetur. Quisque ac mauris volutpat turpis luctus aliquam scelerisque eget dui. Cras porttitor finibus risus, a euismod ipsum porta vitae. Aliquam vehicula, est a laoreet gravida, tellus dui rutrum odio, ac ultrices tellus orci sit amet nisl. Nullam placerat dui et tortor malesuada aliquet. Aliquam laoreet massa non nibh fringilla faucibus.',
         image: {women2}
-    }
+    },
+    {
+        // title: 'Gender Implications of News Output; Reporting the Knowledge Base of Media Practitioners.',
+        date: '17 May 2022',
+        linkText: 'Experts propose mainstreaming of reportage on violence against women report deficit.',
+        link: 'https://guardian.ng/features/media/experts-propose-mainstreaming-of-reportage-on-violence-against-women-report-deficit-2/',
+        image: {women2}
+    },
+    {
+        // title: 'Gender Implications of News Output; Reporting the Knowledge Base of Media Practitioners.',
+        date: '15 April 2022',
+        linkText: 'MEGEIN calls for more women representation in news stories, tasked journalists to uphold codes of the profession.',
+        link: 'https://aprokorepublic.com/megin-calls-for-more-women-representation-in-news-stories-tasked-journalists-to-uphold-codes-of-the-profession',
+        image: {women2}
+    },
 ]
 
 export const projectImages1 = [
@@ -75,5 +120,37 @@ export const projectImages3 = [
     violence7,
     violence8,
     violence9,
+]
+
+export const otherImages = [
+    otherImages1,
+    otherImages2,
+    otherImages3,
+    otherImages4,
+    otherImages5,
+    otherImages6,
+    otherImages7,
+    otherImages8,
+    otherImages9,
+    otherImages10,
+    otherImages11,
+    otherImages12,
+    otherImages13,
+    otherImages14,
+    otherImages15,
+    otherImages16,
+    otherImages17,
+    otherImages18,
+    otherImages19,
+    otherImages20,
+    otherImages21,
+    otherImages22,
+    otherImages23,
+    otherImages24,
+    otherImages25,
+    otherImages26,
+    otherImages27,
+    otherImages28,
+    otherImages29,
 ]
 
